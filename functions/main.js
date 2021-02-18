a6a22bad304c058ab8923a3ca2da3278ee2692cc
Anant <anantcode@gmail.com>
2021-02-18 23:27:39 +0530

    Added secret key support

---
+++ functions/main.js
@@ -0,0 +1,10 @@
+const admin = require('firebase-admin');
+const cors = require('cors')({origin: true});
+const functions = require('firebase-functions');
+
+var serviceAccount = require("functions/service-account.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
